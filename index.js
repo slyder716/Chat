f32bcafce420e4043da5333285421d805edbdbb1
slyder716 <jon_saunders@comcast.net>
2015-11-21 21:38:09 -0700

    Version 1.0

---
+++ index.js
@@ -0,0 +1,24 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+	io.emit('chat message', 'User has connected.');
+	socket.on('chat message', function(msg){
+		io.emit('chat message', msg);
+    	console.log('message: ' + msg);
+  	});
+  	socket.on('disconnect', function(socket){
+		io.emit('chat message', 'User has disconnected.');
+	});
+});
+
+
+http.listen(3000, function(){
+	console.log('listening on *.3000');
+});
